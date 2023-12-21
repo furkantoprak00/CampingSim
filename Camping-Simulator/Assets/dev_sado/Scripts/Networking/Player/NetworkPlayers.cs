271a263fa16944c53c0e5f1ccabfcd33f30eae3b
furkantoprak00 <144796645+furkantoprak00@users.noreply.github.com>
2023-12-21 15:44:07 +0300

    NetworkPlayer CS

---
+++ Camping-Simulator/Assets/dev_sado/Scripts/Networking/Player/NetworkPlayers.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class NetworkPlayers : NetworkBehaviour
+{
+    public GameObject cameraObject;
+    public MonoBehaviour[] localOnlyComponents;
+
+    private void Start()
+    {
+        if (IsLocalPlayer)
+        {
+            cameraObject.SetActive(true);
+            foreach (var component in localOnlyComponents)
+            {
+                component.enabled = true;
+            }
+        }
+        else
+        {
+            cameraObject.SetActive(false);
+            foreach (var component in localOnlyComponents)
+            {
+                component.enabled = false;
+            }
+        }
+    }
+}
